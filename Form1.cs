8181e89de9575389a9aa902b42b94fbbe752503d
Francis King <francis.king@burohappold.com>
2018-09-10 10:50:33 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GoogleDistance
+{
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+        String JavaScriptURL = @"file:///C:/Users/fking/Desktop/JavascriptGoogleMaps.html";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set up the browser
+            webBrowser1.Navigate(JavaScriptURL);
+            webBrowser1.ObjectForScripting = this;
+        }
+
+        public void Test(Single Latitude, Single Longitude)
+        {
+            MessageBox.Show("Latitude: " + Latitude + " , Longitude: " + Longitude);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Clear the markers
+            webBrowser1.Document.InvokeScript("clearMarkers");
+        }
+    }
+}
